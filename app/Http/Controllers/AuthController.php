69aefdbd8484e63f3897287d1367cce39601b409
Dendi Aprilio <gelasajur1209@gmail.com>
2026-08-01 01:50:57 +0700

    Auto-create users on login page if DB is empty (Railway seeder fallback)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,18 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
+        // Auto-seed users if database is empty (fallback for when seeder doesn't run)
+        if (User::count() === 0) {
+            $defaultUsers = [
+                ['name' => 'Akbar', 'email' => 'akbar@spindo.com', 'password' => 'password'],
+                ['name' => 'Reo',   'email' => 'reo@spindo.com',   'password' => 'password'],
+                ['name' => 'Dendi', 'email' => 'dendi@spindo.com', 'password' => 'password'],
+            ];
+            foreach ($defaultUsers as $u) {
+                User::create($u);
+            }
+        }
+
         $users = User::all();
         return view('auth.login', compact('users'));
     }
